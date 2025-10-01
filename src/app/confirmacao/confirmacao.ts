d097b968ffe7c6b26779942628f9e76de4b97c0f
Tony Vandre Klava <tonyklava@gmail.com>
2025-09-30 21:19:05 -0300

    Formatacao do scss da confirmacao

--- src/app/confirmacao/confirmacao.ts
+++ src/app/confirmacao/confirmacao.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Checkbox } from "../utils/checkbox/checkbox";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-confirmacao',
-  imports: [],
+  imports: [FormsModule, Checkbox],
   templateUrl: './confirmacao.html',
   styleUrl: './confirmacao.scss'
 })
 export class Confirmacao {
+  aceitou: boolean = false;
 
 }
---
+++ src/app/utils/checkbox/checkbox.ts
@@ -0,0 +1,59 @@
+import { Component, Input, Output, EventEmitter, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkbox',
+  imports: [],
+  templateUrl: './checkbox.html',
+  styleUrl: './checkbox.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => Checkbox),
+      multi: true
+    }
+  ]
+})
+export class Checkbox implements ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() disabled: boolean = false;
+  @Input() id: string = '';
+  
+  @Output() checkChange = new EventEmitter<boolean>();
+
+  private _checked: boolean = false;
+
+  get checked(): boolean {
+    return this._checked;
+  }
+
+  set checked(value: boolean) {
+    this._checked = value;
+    this.onChange(value);
+    this.checkChange.emit(value);
+  }
+
+  private onChange = (value: boolean) => {};
+  private onTouched = () => {};
+
+  writeValue(value: boolean): void {
+    this._checked = value;
+  }
+
+  registerOnChange(fn: (value: boolean) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onCheckboxChange(event: Event): void {
+    this.checked = !this.checked; 
+    this.onTouched();
+  }
+}
