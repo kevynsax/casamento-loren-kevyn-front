f042544630c7f59c2c2ed05a49408418995ad1e1
C1317023 Kevyn Pinheiro Klava <kevynsax@gmail.com>
2025-11-03 21:46:41 -0300

    Add Home page; Add menu component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Convite } from './pages/convite/convite';
 import { Confirmacao } from './pages/confirmacao/confirmacao';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-      { path: 'convite/:id', component: Convite},
-      { path: 'confirmacao/:id', component: Confirmacao},
+    { path: '', component: Home },
+    { path: 'convite/:id', component: Convite },
+    { path: 'confirmacao/:id', component: Confirmacao },
 ];
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Menu } from '../../menu/menu';
+
+@Component({
+  selector: 'app-home',
+    imports: [
+        Menu
+    ],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+
+}
