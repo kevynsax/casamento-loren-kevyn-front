9700c88f196512c91ce4a23c6eb48a63a5990e74
kevyn klava <kevynsax@gmail.com>
2026-01-10 16:56:48 -0300

    Update ceremony date constants: include timezone information and increment version

--- src/service/constants.ts
+++ src/service/constants.ts
@@ -1,3 +1,3 @@
-export const DATA_CASAMENTO = new Date('2026-01-25');
-export const INICIO_CERIMONIA = new Date('2026-01-25T10:30:00');
-export const FIM_CERIMONIA = new Date('2026-01-25T17:30:00');
+export const DATA_CASAMENTO = new Date('2026-01-25T00:00:00.000-03:00');
+export const INICIO_CERIMONIA = new Date('2026-01-25T10:30:00.000-03:00');
+export const FIM_CERIMONIA = new Date('2026-01-25T17:30:00.000-03:00');
