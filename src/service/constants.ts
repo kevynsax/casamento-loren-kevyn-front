4e420b80331d8823485f58f3969d4cdf7bf86a55
kevyn klava <kevynsax@gmail.com>
2026-01-10 16:48:14 -0300

    Update wedding date and ceremony times: change event date to January 25, 2026

--- src/service/constants.ts
+++ src/service/constants.ts
@@ -1,3 +1,3 @@
-export const DATA_CASAMENTO = new Date('2026-01-26');
-export const INICIO_CERIMONIA = new Date('2026-01-26T10:30:00');
-export const FIM_CERIMONIA = new Date('2026-01-26T17:30:00');
+export const DATA_CASAMENTO = new Date('2026-01-25');
+export const INICIO_CERIMONIA = new Date('2026-01-25T10:30:00');
+export const FIM_CERIMONIA = new Date('2026-01-25T17:30:00');
